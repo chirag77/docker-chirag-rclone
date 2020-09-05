8a3d2cd9adba5e7454d117315c1d4c34acd09e18
Chirag <chiragconnect6174@gmail.com>
2020-09-05 15:43:01 +0530

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,7 +13,7 @@ pwd
 ls
 
 
-rclone --config=`find . -name "rclone.conf"` copy lr_other:db_OTHER_coursework/Deep_Knowledge_DK_dkprime/index_pages/lists_html/3/Urgency_of_change___dkprime_2020-05-09_6_11_52_PM.html .
+rclone --config=`find . -name ./builds/chirag314/test_rclone/config/rclone.conf copy lr_other:db_OTHER_coursework/Deep_Knowledge_DK_dkprime/index_pages/lists_html/3/Urgency_of_change___dkprime_2020-05-09_6_11_52_PM.html .
 
 ls
 
